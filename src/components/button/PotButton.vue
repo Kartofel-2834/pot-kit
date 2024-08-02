5e2be67df6b6079e8eb6674fae512c549a3d9bda
Radjab Gabibov <poko2834@mail.ru>
2024-08-02 11:13:23 +0300

    feat(init): inited project

---
+++ src/components/button/PotButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <component
+        :is="tag"
+        :class="$style.PotButton"
+    >
+    </component>
+</template>
+
+<style lang="scss" module>
+.PotButton {
+    //
+}
+</style>
