68774ac70456a9ef52c837b6cbd49aa3ac0e8e3f
Radjab Gabibov <poko2834@mail.ru>
2025-05-07 11:35:19 +0300

    feat(index): repository cleaning

--- lib/types/pot-kit/composables/device-properties.ts
+++
@@ -1,5 +0,0 @@
-export type TDevicePropertyValue<T> = T extends unknown[] ? TDevicePropertyValue<T[number]> : T;
-
-export type TDeviceProperties<T> = {
-    [Property in keyof T]: TDevicePropertyValue<T[Property]>;
-}
